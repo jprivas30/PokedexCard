faf126917913f84808f89b3930b9cdd257359f0d
Juan Rivas <juanpr2512@gmail.com>
2022-11-15 00:01:02 -0300

    Tarea6

---
+++ assets/js/script.js
@@ -0,0 +1,399 @@
+// CARD1
+
+const dataApiPokemon1 = async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/1/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke0.setAttribute('src',img1)
+    namePoke0.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke0 = document.querySelector('[name-Poke0]')
+const imgPoke0 = document.querySelector('[img-Poke0]')
+
+dataApiPokemon1()
+
+// CARD2
+
+const dataApiPokemon2= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/2/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke1.setAttribute('src',img1)
+    namePoke1.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke1 = document.querySelector('[name-Poke1]')
+const imgPoke1 = document.querySelector('[img-Poke1]')
+
+dataApiPokemon2()
+
+// CARD3
+
+const dataApiPokemon3= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/3/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke2.setAttribute('src',img1)
+    namePoke2.textContent= name1
+  } catch (error) {
+    
+  }
+}
+const namePoke2 = document.querySelector('[name-Poke2]')
+const imgPoke2 = document.querySelector('[img-Poke2]')
+
+dataApiPokemon3()
+
+// CARD4
+
+const dataApiPokemon4= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/4/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke3.setAttribute('src',img1)
+    namePoke3.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke3 = document.querySelector('[name-Poke3]')
+const imgPoke3 = document.querySelector('[img-Poke3]')
+
+dataApiPokemon4()
+
+// CARD5
+
+const dataApiPokemon5= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/5/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke4.setAttribute('src',img1)
+    namePoke4.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke4 = document.querySelector('[name-Poke4]')
+const imgPoke4 = document.querySelector('[img-Poke4]')
+
+dataApiPokemon5()
+
+// CARD6
+
+const dataApiPokemon6= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/6/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke5.setAttribute('src',img1)
+    namePoke5.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke5 = document.querySelector('[name-Poke5]')
+const imgPoke5 = document.querySelector('[img-Poke5]')
+
+dataApiPokemon6()
+
+// CARD7
+
+const dataApiPokemon7= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/7/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke6.setAttribute('src',img1)
+    namePoke6.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke6 = document.querySelector('[name-Poke6]')
+const imgPoke6 = document.querySelector('[img-Poke6]')
+
+dataApiPokemon7()
+
+// CARD8
+
+const dataApiPokemon8= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/8/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke7.setAttribute('src',img1)
+    namePoke7.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke7 = document.querySelector('[name-Poke7]')
+const imgPoke7 = document.querySelector('[img-Poke7]')
+
+dataApiPokemon8()
+
+// CARD9
+
+const dataApiPokemon9= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/9/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke8.setAttribute('src',img1)
+    namePoke8.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke8 = document.querySelector('[name-Poke8]')
+const imgPoke8 = document.querySelector('[img-Poke8]')
+
+dataApiPokemon9()
+
+// CARD10
+
+const dataApiPokemon10= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/10/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke9.setAttribute('src',img1)
+    namePoke9.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke9 = document.querySelector('[name-Poke9]')
+const imgPoke9 = document.querySelector('[img-Poke9]')
+
+dataApiPokemon10()
+// // CARD11
+
+const dataApiPokemon11= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/11/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke10.setAttribute('src',img1)
+    namePoke10.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke10 = document.querySelector('[name-Poke10]')
+const imgPoke10 = document.querySelector('[img-Poke10]')
+
+dataApiPokemon11()
+
+// CARD12
+
+const dataApiPokemon12= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/12/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke11.setAttribute('src',img1)
+    namePoke11.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke11 = document.querySelector('[name-Poke11]')
+const imgPoke11 = document.querySelector('[img-Poke11]')
+
+dataApiPokemon12()
+
+// CARD13
+
+const dataApiPokemon13= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/13/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke12.setAttribute('src',img1)
+    namePoke12.textContent= name1
+  } catch (error) {
+    
+  }
+}
+const namePoke12 = document.querySelector('[name-Poke12]')
+const imgPoke12 = document.querySelector('[img-Poke12]')
+
+dataApiPokemon13()
+
+// CARD14
+
+const dataApiPokemon14= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/14/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke13.setAttribute('src',img1)
+    namePoke13.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke13 = document.querySelector('[name-Poke13]')
+const imgPoke13 = document.querySelector('[img-Poke13]')
+
+dataApiPokemon14()
+
+// CARD15
+
+const dataApiPokemon15= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/15/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke14.setAttribute('src',img1)
+    namePoke14.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke14 = document.querySelector('[name-Poke14]')
+const imgPoke14 = document.querySelector('[img-Poke14]')
+
+dataApiPokemon15()
+
+// CARD16
+
+const dataApiPokemon16= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/16/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke15.setAttribute('src',img1)
+    namePoke15.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke15 = document.querySelector('[name-Poke15]')
+const imgPoke15 = document.querySelector('[img-Poke15]')
+
+dataApiPokemon16()
+
+// CARD17
+
+const dataApiPokemon17= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/17/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke16.setAttribute('src',img1)
+    namePoke16.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke16 = document.querySelector('[name-Poke16]')
+const imgPoke16 = document.querySelector('[img-Poke16]')
+
+dataApiPokemon17()
+
+// CARD18
+
+const dataApiPokemon18= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/18/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke17.setAttribute('src',img1)
+    namePoke17.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke17 = document.querySelector('[name-Poke17]')
+const imgPoke17 = document.querySelector('[img-Poke17]')
+
+dataApiPokemon18()
+
+// CARD19
+
+const dataApiPokemon19= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/19/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke18.setAttribute('src',img1)
+    namePoke18.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke18 = document.querySelector('[name-Poke18]')
+const imgPoke18= document.querySelector('[img-Poke18]')
+
+dataApiPokemon19()
+
+// CARD20
+
+const dataApiPokemon20= async () => {
+  try { 
+    const resultado = await fetch ('https://pokeapi.co/api/v2/pokemon/20/')
+    const data1 = await resultado.json();
+    const img1 = data1.sprites.other.dream_world.front_default
+    const name1 = data1.species.name
+    imgPoke19.setAttribute('src',img1)
+    namePoke19.textContent= name1
+  } catch (error) {
+    
+  }
+}
+
+const namePoke19 = document.querySelector('[name-Poke19]')
+const imgPoke19= document.querySelector('[img-Poke19]')
+
+dataApiPokemon20()
+
+
+
